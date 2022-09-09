4601542428e7491b5d6f1bfea86bb06fc143678a
Daberechi <daberelevi@gmail.com>
2022-09-09 14:59:12 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
+
+
+
 /**
- *main -Entry point
- *Return:Always 0 (Success)
- */
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0
+
+*/
+
 int main(void)
+
 {
-       puts("\"Programming is like  building a multilingual puzzle");
-       return (0);
-}
+
+	puts("\"programming is like building a multilingual puzzle");
+
+	return (0);
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main -Entry poinr
+ * Return:Always 0 (Success)
+ */
+init main(void)
+{
+        printf("Size of a char: %d bytr(s)\n", sizeof(char));
