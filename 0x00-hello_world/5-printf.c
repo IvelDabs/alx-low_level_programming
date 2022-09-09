fe61da7f26984905875c8bd8d43fc35cd7f0aa2e
Daberechi <daberelevi@gmail.com>
2022-09-08 18:45:58 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multi lingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+     pritf("with proper grammer, but the outcome is a piece of art,\n");
+     return: 0
+}
