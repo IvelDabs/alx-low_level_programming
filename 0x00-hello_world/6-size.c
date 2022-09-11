792344a521a22d29dd2ac3204a9def35479e6ca5
Daberechi <daberelevi@gmail.com>
2022-09-11 11:11:17 -0700

    Task 0

--- 0x00-hello_world/6-size.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-/**
- * main -Entry poinr
- * Return:Always 0 (Success)
- */
-init main(void)
-{
-        printf("Size of a char: %d bytr(s)\n", sizeof(char));
---
+++ README.md
@@ -0,0 +1,1 @@
+This preliminary tasks are all C low level language oriented and within the contents of all the files will be different scripts to run different tasks
