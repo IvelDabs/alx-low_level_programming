ea82720cd9e41a8d31d35747c1490a3108839d95
Daberechi <daberelevi@gmail.com>
2022-09-13 14:51:02 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print abc
+ * author: Dr_savantcode
+ * Return: 0 0n success
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
