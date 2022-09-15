c0cdb96bb8f0133a21de2d1c6e2312a8b296c5e8
Daberechi <daberelevi@gmail.com>
2022-09-15 08:30:08 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * main - function to print if integer is
+ * positive or negative
+ *
+ * task: 0
+ * author: Dr-savantcode
+ * Return: 0 on success
+ */
+
+
+int main(void)
+{
+int i;
+
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
+
+}
